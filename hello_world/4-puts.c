b4cb1686f215ba98275d021e28560bc5b1b6397e
SpStigma <GitHubStigma@gmail.com>
2023-06-14 05:20:12 -0700

    correction r to R

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - foction de base a mettre a chaque fois
- * return: pour savoir si c bon
+ * Return: to succes
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
