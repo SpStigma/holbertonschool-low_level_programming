ae9f063f0159d6c4af758afc240fed664e0ef71c
SpStigma <GitHubStigma@gmail.com>
2023-06-14 05:01:37 -0700

    command puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - foction de base a mettre a chaque fois
+ * return: pour savoir si c bon
+ */
+int main(void)
+{
+puts('"'Programming is like building a multilingual puzzle);
+return 0;
+}
