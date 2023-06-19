73e37055cf9589c40015352508899e543599f70b
SpStigma <GitHubStigma@gmail.com>
2023-06-19 05:32:49 -0700

    afficher les premier digits

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print pour afficher les premiers digits
+ * Return: to sucess
+ */
+
+int main(void)
+{
+	int start = 0;
+
+		for (; start <= 9 ; start++)
+	{
+		printf("%d", start);
+	}
+		{
+			printf("\n")
+		}
+	return (0);
+}
+
