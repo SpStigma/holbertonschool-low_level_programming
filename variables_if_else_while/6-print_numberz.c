8da750a4dca413804e4086c4869e734060ba2cfd
SpStigma <GitHubStigma@gmail.com>
2023-06-19 05:54:02 -0700

    les premiers digit avec contraintes

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - les premiers digits avec des contraintes
+ * Return: to sucess
+ */
+
+int main(void)
+{
+	int start = 0;
+
+	for (; start <= 9 ; start++)
+	{
+		putchar(start);
+	}
+	putchar('\n');
+	return (0);
+}
