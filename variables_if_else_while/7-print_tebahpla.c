5244d284baaf43186e474ef18aeef832a2ed9169
SpStigma <GitHubStigma@gmail.com>
2023-06-19 06:27:34 -0700

    alphabet a l'envers test 1

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet a l'envers
+ * Return: to sucess
+ */
+
+int main(void)
+{
+	int reverse = 'z';
+
+	for (; reverse <= 'a' ; reverse++)
+	{
+		putchar(reverse);
+	}
+	return (0);
+}
