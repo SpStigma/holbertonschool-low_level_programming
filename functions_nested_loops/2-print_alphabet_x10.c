31574888eeb20c9d5cb832078fe8610cd2535d8f
SpStigma <GitHubStigma@gmail.com>
2023-06-20 00:05:52 -0700

    print 10 first alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the ten first letter of alphabet
+ */
+
+print_alphabet_x10(void)
+{
+	char mid;
+
+	for (mid = 'a' ; mid <= 'j' ; mid++)
+	{
+		_putchar(mid);
+	}
+	_putchar('\n');
+}
+
