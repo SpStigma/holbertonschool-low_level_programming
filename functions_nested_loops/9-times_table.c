845d0e7b7117471f0394a6c93d7d731ce5f07e00
SpStigma <GitHubStigma@gmail.com>
2023-06-20 11:18:53 -0700

    nouveau try

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,11 +8,19 @@
 void times_table(void)
 {
 	int n;
-		for (n = 0 ; n < 10 ; n++)
+	int m;
+
+		for (n = 0 ; n <= 9 ; n++)
 		{
-			int result = 9 * n;
+			for (m = 0 ; m <= 10 ; m++)
+			{
+			int result = n * m;
 
 			_putchar(result + '0');
-			_putchar('\n');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			}
 		}
+		_putchar('\n');
 }
