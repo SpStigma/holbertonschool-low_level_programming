1f1b1926309da4638e48fc111bbfb20645458797
SpStigma <GitHubStigma@gmail.com>
2023-06-20 07:37:57 -0700

    last digit negative or positive

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function to print last digit
+ *@number: tout les nombres
+ *
+ *Return: last number
+ */
+
+int print_last_digit(int number)
+{
+	int last
+		if (n < 0)
+		{
+			last = number % 10 * -1;
+			_putchar(last +'0');
+			return (last);
+		}
+		else
+		{
+			last = number % 10;
+			_putchar(last +'0')
+				return (last);
+		}
+
+}
+
