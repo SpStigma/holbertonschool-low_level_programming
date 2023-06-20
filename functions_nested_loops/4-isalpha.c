e722df32b7054d0d6a9399807ef87bae1d176f6d
SpStigma <GitHubStigma@gmail.com>
2023-06-20 04:52:50 -0700

    all alphabet

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - identify lower and upper case
+ * @c: all character
+ *
+ * Return: to sucess if 1.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+	       	return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
