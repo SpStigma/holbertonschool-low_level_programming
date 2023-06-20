cfcdb260b1c3168ff3431c191b9eaa9e206c7ef4
SpStigma <GitHubStigma@gmail.com>
2023-06-20 08:43:03 -0700

    table de 9 non répéter

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * times_table - table de 9 repéter 9 fois
+ */
+
+void times_table(void)
+{
+	int n;
+		for (n = 0 ; n < 10 ; n++)
+		{
+			int result = 9 * n;
+
+			_putchar(result);
+			_putchar('\n');
+		}
+}
