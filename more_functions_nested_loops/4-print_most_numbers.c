698faa517ad69f67cbb4d2400bd232054472466a
SpStigma <GitHubStigma@gmail.com>
2023-06-21 12:08:20 -0700

    fix: betty norm

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *
+ * print_most_numbers - print de 1 a 9 execpt 2 and 4
  *
  */
 
@@ -9,9 +9,9 @@ void print_most_numbers(void)
 {
 	int n = '0';
 
-	for (; n <= '9' ; n++);
+	for (; n <= '9' ; n++)
 	{
-		if ( n != '2' && n != '4')
+		if (n != '2' && n != '4')
 		{
 			_putchar(n);
 		}
