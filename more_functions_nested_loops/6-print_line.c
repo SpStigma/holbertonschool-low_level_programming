e86ce33cb5af9c6768c0486508137cf7736f45ec
SpStigma <GitHubStigma@gmail.com>
2023-06-21 12:37:12 -0700

    straight line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - nombre de fois pour mettre _
+ *@n: la valeur a laquelle repeat tend
+ *
+ */
+
+void print_line(int n)
+{
+	int n;
+	int repeat = 0;
+
+	for (repeat ; repeat < n ; repeat++)
+	{
+		_putchar('_');
+	}
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
