b054c9dbb556eefd28a91338db54caac7a6e9bbc
SpStigma <GitHubStigma@gmail.com>
2023-06-21 11:43:07 -0700

    résultat multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplication de 2 entier
+ * @a: nombre a multiplier
+ * @b: multiplicateur
+ *
+ * Return: resultat
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
