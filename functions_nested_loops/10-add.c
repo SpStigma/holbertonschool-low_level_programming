eeb9a447223b4348d0f5f35358d0d88b85725aeb
SpStigma <GitHubStigma@gmail.com>
2023-06-21 00:57:46 -0700

    fix: betty style

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,11 +2,13 @@
 
 /**
  * add - somme de 2 entiers
+ *@a: premier entier à additioner
+ *@b: premier entier à additionner
  *
- *
+ *Return: somme des 2 entiers
  */
 
 int add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
