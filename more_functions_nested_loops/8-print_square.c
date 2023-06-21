615de966666db9a3dd968e4767b4a1c415e8614c
SpStigma <GitHubStigma@gmail.com>
2023-06-21 13:59:52 -0700

    carré avec l et c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void print_square(int size)
+{
+	int l;
+	int c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (l = 0 ; l < size ; l++)
+	{
+		for (c = 0 ; c < size ; c++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
+
+
