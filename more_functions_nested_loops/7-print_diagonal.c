103f249a6785bba1e0a8646c3f5870164fc6bc3c
SpStigma <GitHubStigma@gmail.com>
2023-06-21 12:55:23 -0700

    diagonale

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - display a diagonal on the terminal
+ * @n: longueur de la diagonale
+ */
+
+void print_diagonal(int n)
+{
+	int a;
+
+	for (a = 0 ; a <= n ; a++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('\');
+		_putchar('\n');
+	}
+}
