1c6a30f09eaa51a5f70903ab38f8bce12141139b
SpStigma <GitHubStigma@gmail.com>
2023-06-22 04:40:51 -0700

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * main - ecrie si n multiple de 5 et 3
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (; n <= 100 ; n++)
+	{
+		printf(" ");
+
+		if ( n % 3)
+			printf("Fizz");
+	}
+	if (n % 5)
+	{
+		printf("Buzz");
+	}
+	if (n % 3 && n % 5)
+	{
+		printf("FizzBuzz");
+	}
+}
