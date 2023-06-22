51dd0a9778812df5c76d3c41426102549c1ebdcf
SpStigma <GitHubStigma@gmail.com>
2023-06-22 11:37:39 -0700

    1h de travaille prey to sucess

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,9 @@
 
 void print_to_98(int n)
 {
-	 for (n ; n <= 98 ; n++)
+	if (n <= 98)
+	{
+	 for (; n <= 98 ; n++)
 	 {
 		 printf("%d", n);
 
@@ -17,9 +19,10 @@ void print_to_98(int n)
 			 printf(", ");
 		 }
 	 }
+	}
 	 else
 	 {
-		 for (n ; n >= 98 ; n--)
+		 for (; n >= 98 ; n--)
 		 {
 			 printf("%d", n);
 			 if (n != 98)
