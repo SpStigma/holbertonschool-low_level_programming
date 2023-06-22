0b4a8c71ac2db4e5d85250ed09cab47e22aec4d1
SpStigma <GitHubStigma@gmail.com>
2023-06-22 10:33:51 -0700

    fix: add return if < 0

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,13 @@ void print_triangle(int size)
 	int i;
 	int j;
 
-	for (i = 0 ; i <= size ; i++)
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1 ; i <= size ; i++)
 	{
 		for (j = 1 ; j <= i ; j++)
 		{
