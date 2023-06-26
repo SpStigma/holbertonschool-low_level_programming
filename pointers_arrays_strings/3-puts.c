79eb7d6156a95926adad64e06aa4c41a77da956e
SpStigma <GitHubStigma@gmail.com>
2023-06-26 02:50:52 -0700

    try

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - affiche la phrase
+ * @str: la chaine de caractère a imprimer
+ *
+ *Return: charactère
+ */
+
+void _puts(char *str)
+{
+	char c;
+
+	for (str ; str[c] != 0 ; c++)
+	{
+	}
+	return (c);
+	_putchar('\n');
+}
