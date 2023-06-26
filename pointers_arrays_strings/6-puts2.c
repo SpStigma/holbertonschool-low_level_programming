2fd1718a6e34d75fb27b4db82fd296ec4ce95a03
SpStigma <GitHubStigma@gmail.com>
2023-06-26 11:33:04 -0700

    utilisation de modulo de 2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * puts2 - imprimer un caractère 1 fois sur 2
+ * @str: cahine de caractère
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
