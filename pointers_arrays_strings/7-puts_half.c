6e7eb9af46c7fbf92c0824231c287d9a17e6aa54
SpStigma <GitHubStigma@gmail.com>
2023-06-26 12:44:14 -0700

    imprimé a partir du milieu

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+ * puts_half - print la moitié des caractère
+ * @str: chaine de caractère
+ */
+
+void puts_half(char *str)
+{
+	int lenght = 0;
+	int i = 0;
+	int start;
+
+	while (s[lenght] != '\0')
+	{
+		lenght++;
+	}
+	if (lenght % 2 == 0)
+	{
+		start = lenght / 2;
+	}
+	else
+	{
+		start = (lenght + 1) / 2;
+	}
+	for (i = start ; i < lenght ; i++)
+	{
+		_putchar(str[i];
+	}
+	_putchar('\n');
+}
