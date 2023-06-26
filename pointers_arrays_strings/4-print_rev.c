1d6506f6266e8cfd2b99dabaa3ee348c63bc9126
SpStigma <GitHubStigma@gmail.com>
2023-06-26 05:25:34 -0700

    reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int lenght = 0;
+
+	while (s[lenght] != '0')
+		lenght++;
+	for (i = lenght -1 ; i >= 0 ; i--)
+		_putchar(s[i];
+		putchar('\n');
+}		
