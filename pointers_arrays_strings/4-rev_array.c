90bbb4ec4e0084150acb6c13546e92562789571a
SpStigma <GitHubStigma@gmail.com>
2023-06-27 12:42:41 -0700

    array reverse

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse l'ordre ds l'array
+ * @a: array
+ * @n: nombre ds l'array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int debut = 0;
+	int fin = n - 1;
+	int temp;
+
+	while (start < end)
+	{
+		temp = a[debut];
+		a[start] = a[end];
+		a[end] = temp;
+
+		debut++;
+		fin--;
+
+	}
+}
