88a8782deb709c1204d76f4c72dcc924f09cf0e4
SpStigma <GitHubStigma@gmail.com>
2023-06-27 05:10:04 -0700

    fix:betty doc

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main."
+
+/**
+ * *_strncat - function that concatenates two strings.
+ * @dest: premier string
+ * @src: deuxième string
+ * @n: a ou s'arrreter
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int lenght;
+
+	while (dest[lenght] != '\0')
+	{
+		lenght++;
+	}
+	for (i = 0 ; i < n && scrc[i] != '\0' ; i++)
+	{
+		dest[lenght + 1] = src[i];
+		dest[lenght + i] = '\0';
+	}
+	return (dest);
+}
