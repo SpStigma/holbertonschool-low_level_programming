792e5cb2d5680a7382be68e4054dbbe9e1a985ab
SpStigma <GitHubStigma@gmail.com>
2023-06-28 04:23:22 -0700

    function that capitalizes all words of a string.

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *cap_string - upper every chaacter
+ * @str: phrase a afficher
+ *
+ * Return: return tout les mots avec une maj au début
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			if
+				((str[i - 1] == ',') ||
+				 (str[i - 1] == ';') ||
+				 (str[i - 1] == '.') ||
+				 (str[i - 1] == '!') ||
+				 (str[i - 1] == '?') ||
+				 (str[i - 1] == '(') ||
+				 (str[i - 1] == ')') ||
+				 (str[i - 1] == '{') ||
+				 (str[i - 1] == '}') ||
+				 (str[i - 1] == '\n') ||
+				 (str[i - 1] == '\t'))
+					str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
