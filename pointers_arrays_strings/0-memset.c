4e498492968baf7b576be48d44557d4a36b23a29
SpStigma <GitHubStigma@gmail.com>
2023-06-28 11:00:19 -0700

    fix: betty style

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,8 +1,12 @@
 #include "main.h"
 
 /**
+ * *_memset - function that fills memory with a constant byte
+ * @s: pointer s
+ * @b: constant bytes
+ * @n: the number of bytes to fill
  *
- *
+ * Return: s
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -10,7 +14,7 @@ char *_memset(char *s, char b, unsigned int n)
 	char *ptr = s;
 
 	while (n > 0)
-       	{
+	{
 		*ptr = b;
 		ptr++;
 		n--;
