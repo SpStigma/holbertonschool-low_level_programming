c1efab6f241c7246fcee9f902db6a9bb77a83dfe
SpStigma <GitHubStigma@gmail.com>
2023-06-28 04:26:56 -0700

    fix: add a new condition to i = 0

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -26,7 +26,8 @@ char *cap_string(char *str)
 				 (str[i - 1] == '{') ||
 				 (str[i - 1] == '}') ||
 				 (str[i - 1] == '\n') ||
-				 (str[i - 1] == '\t'))
+				 (str[i - 1] == '\t') ||
+				 (str[i] == 0)
 					str[i] = str[i] - 32;
 		}
 		i++;
