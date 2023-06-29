14e8c11610a824bb24457c7444e404221bdb5b27
SpStigma <GitHubStigma@gmail.com>
2023-06-29 06:11:11 -0700

    function that locates a character in a string.

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strchr - Write a function that locates a character in a string.
+ * @s: chaine de character
+ * @c: caractère a trouver
+ *
+ * Return: to result
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s[i]);
+			i++;
+	}
+	else
+	{
+		return (NULL);
+	}
+}
