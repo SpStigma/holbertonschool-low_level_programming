b42f2adba8c95f3eda776e0de02e1d99b9b8fc47
SpStigma <GitHubStigma@gmail.com>
2023-06-29 10:22:20 -0700

    fix: changement de tout

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,13 +11,22 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i = 0;
+	int j = 0;
 	int count = 0;
 
-	while (s[i] != '\0' && accept[count] != '\0')
+	while (s[i] != '\0')
 	{
-		if (s[i] == accept[count])
+		for (j = 0 ; accept[j] != '\0' ; j++)
 		{
-			count++;
+			if (s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			return (count);
 		}
 		i++;
 	}
