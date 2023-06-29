3e8720920203a84da121ecb60601da9a97353d3d
SpStigma <GitHubStigma@gmail.com>
2023-06-29 12:46:54 -0700

    chess for life

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - affiche les pièce de chess
+ * @a: tableau de 8
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int l = 0;
+	int c = 0;
+
+	for (l = 0 ; l < 8 ; l++)
+	{
+		for (c = 0 ; c < 8 ; c++)
+		{
+			_putchar(a[l][c]);
+			_putchar('\n');
+		}
+	}
+}
