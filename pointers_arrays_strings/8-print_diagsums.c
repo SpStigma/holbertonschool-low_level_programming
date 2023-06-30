6ea9a219e71389ba49783f38147f7fe477dd1424
SpStigma <GitHubStigma@gmail.com>
2023-06-30 06:17:18 -0700

    print sum diagonale

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - sum des diagonale
+ * @a: matrix
+ * @size: taille de la matrixe
+ *
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int l = 0;
+	int c = 0;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	while (l < size)
+	{
+		sum1 += *(a + l * size + c);
+		sum2 += *(a + l * size + (size - 1 - c));
+
+		l++;
+		c++;
+	}
+	printf("%d, %d", sum1, sum2);
+}
