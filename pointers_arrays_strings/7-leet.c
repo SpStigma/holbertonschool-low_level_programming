f1de9eb632c0ae67d077cbdc5a874b502314f85b
SpStigma <GitHubStigma@gmail.com>
2023-06-30 09:33:17 -0700

    First attempt to string into 1337 thx Fab

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *leet - encode to 1337
+ * @s: string to analyse
+ *
+ * Return: string to 1337
+ */
+
+
+
+char *leet(char *s)
+{
+	char id[] = {'a', 'e', 'o', 't', 'l'};
+	char code[] = {'4', '3', '0', '7', '1'};
+	int lenght = 0;
+	int i = 0;
+
+	while (s[lenght] != '\0')
+	{
+		for (i = 0 ; code[i] != '\0' ; i++)
+		{
+			if (s[lenght] == letters[i] || s[lenght] == letters[i] - 32)
+			{
+				s[lenght] = code[i];
+			}
+		}
+		lenght++;
+	}
+
+	return (s);
+}
