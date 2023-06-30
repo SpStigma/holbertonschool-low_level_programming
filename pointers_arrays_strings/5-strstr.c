685fb11ecb5003b4da57ce3e907941c595b80066
SpStigma <GitHubStigma@gmail.com>
2023-06-30 02:49:18 -0700

    fix: tout le code by my peers

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,18 +12,18 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
 
-	if (*needle == '\0')
+	while (haystack[i] != '\0')
 	{
-		return (haystack);
-	}
+		i = 0;
 
-	while (haystack[i]!= '\0' && needle[i] != '\0' && haystack[i] == needle[i])
-	{
-		i++;
-
-		if (needle[i] == '\0')
+		while (needle[i] != '\0' && haystack[i] == needle[i])
 		{
-			return (haystack);
+			i++;
+
+			if (needle[i] == '\0')
+			{
+				return (haystack);
+			}
 		}
 		haystack++;
 	}
