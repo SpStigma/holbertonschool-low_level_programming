b6f8045b05efbf1efdc9457b81be418f43ddb20e
SpStigma <GitHubStigma@gmail.com>
2023-07-03 02:23:05 -0700

    Fonction recursive

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - unction that prints a string, followed by a new line.
+ * @s: String to be analyse
+ *
+ * Return: string s
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+		_putchar(*s);
+		s++;
+	return (*s);
+}
