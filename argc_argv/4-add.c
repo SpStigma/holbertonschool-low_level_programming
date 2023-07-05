20553e243e43b1272949762abdd9c75acaa9b4a0
SpStigma <GitHubStigma@gmail.com>
2023-07-05 05:59:59 -0700

    add n number new version

---
+++ argc_argv/4-add.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - add n number
+ *
+ * @argc: nombre auxquel i vas tendre
+ * @argv: affichage
+ *
+ * Return: to sucess
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int sum = 0;
+
+
+	for (j = 1 ; j < argc ; j++)
+	{
+		char *arg = argv[j];
+		int isDigit = 1 ;
+		for ( i = 0 ; arg[i] != '\0' ; i++)
+		{
+			if (!isdigit(arg[i]))
+			{
+				isDigit = 0;
+				break;
+			}
+		}
+		if (isDigit)
+		{
+			sum += atoi(arg);
+		}
+		else
+		{
+			printf("Error");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
