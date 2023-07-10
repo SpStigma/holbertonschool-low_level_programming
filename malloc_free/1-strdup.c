6762fc782ee1c1aa4dbd3bd5ce8867c092c8fdd1
SpStigma <GitHubStigma@gmail.com>
2023-07-10 05:09:10 -0700

    first attempt to duplicate

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup -
+ * @str: sting to be dupplicated
+ *
+ * Return: duplicate
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate = NULL;
+	int i;
+	int j;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+
+	while (str[i] != 0)
+	{
+		i++;
+	}
+	duplicate = malloc(sizeof(char) * (i + 1));
+
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; j < i ; j++)
+	{
+		duplicate[j] = str[j];
+	}
+	duplicate[j] = '\0';
+	return (duplicate);
+}
