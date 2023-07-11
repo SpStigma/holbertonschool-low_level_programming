fd92733f69b2a59ee2f087ae82680b61556d6937
SpStigma <GitHubStigma@gmail.com>
2023-07-11 06:02:24 -0700

    fix: int a char

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,24 +12,24 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int total = nmemb * size;
-	unsigned int *ptr = NULL;
-	unsigned int *byteptr = NULL;
+	unsigned char *ptr = NULL;
 	unsigned int i;
 
-	  if ((nmemb == 0) || (size == 0))
-	  {
-		  return (NULL);
-	  }
+	if ((nmemb == 0) || (size == 0))
+	{
+		return (NULL);
+	}
 
-	  ptr = malloc(total);
+	ptr = malloc(total);
 
-	  if (ptr == NULL)
-	  {
-		  byteptr = ptr;
-		  for (i = 0; i < total; i++)
-		  {
-			byteptr[i] = 0;
-		  }
-	  }
-	  return (ptr);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < total; i++)
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
 }
