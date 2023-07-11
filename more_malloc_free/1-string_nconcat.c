7183b5b1bb9c809470d1031e83c084af50164694
SpStigma <GitHubStigma@gmail.com>
2023-07-11 01:35:37 -0700

    concat with n bytes of s2

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat
+ *
+ *
+ *
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int sl1 = 0, sl2 = 0, i = 0, j = 0;
+	char *conca = NULL;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[sl1] != '\0')
+		sl1++;
+	while (s2[sl2] != '\0')
+		sl2++;
+
+	conca = malloc(sizeof(char) * (sl1 + n + 1));
+
+	if (conca == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		conca[i] = s1[i];
+		i++;
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		conca[i] = s2[j];
+		i++;
+	}
+	conca[i] = '\0';
+	return (conca);
+}
