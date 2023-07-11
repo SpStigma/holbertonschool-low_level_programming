924cce8d1e6549120a2b0e9eb8014c37cf0ee3e2
SpStigma <GitHubStigma@gmail.com>
2023-07-11 00:39:02 -0700

    first attempt to check malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - malloc check
+ * @b: number ti multiply
+ * 
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = NULL;
+	 ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
