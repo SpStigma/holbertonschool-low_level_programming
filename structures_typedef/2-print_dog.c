38420604223e26550b10ef0dfdafc17d8b217395
SpStigma <GitHubStigma@gmail.com>
2023-07-17 00:36:04 -0700

    first attempt to print a struct

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d: struct to print
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (d->name == NULL)
+		{
+			printf("NAME: (nil)\n");
+		}
+		else
+		{
+			printf("NAME: %s\n",d->name);
+		}
+		  if (d->age < 0)
+                {
+                        printf("AGE: (nil)\n");
+                }
+                else
+                {
+                        printf("AGE: %f\n", d->age);
+                }
+		if (d->owner == NULL)
+		{
+			printf("OWNER: (nil)\n");
+		}
+		else
+		{
+			printf("OWNER: %s\n",d->owner);
+		}
+	}
+}
