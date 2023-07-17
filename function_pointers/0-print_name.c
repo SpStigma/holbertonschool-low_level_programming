ad830893967bf0626f454c35213bff93fde1ac3a
SpStigma <GitHubStigma@gmail.com>
2023-07-17 16:01:47 -0700

    first attempt to use a fct pointers to print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print name with function pointer
+ * @name: name to print
+ * @f: fct ponter to take the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
