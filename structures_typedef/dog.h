22e63f4f98f4ead4463c304db50716ea2737ee11
SpStigma <GitHubStigma@gmail.com>
2023-07-16 21:56:17 -0700

    fix: betty doc

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - identity of a dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: identity of a dog with name age owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-	/* struct identity of a dog */
 };
 
 #endif
