64582f2355a1bc8d5e04758d834bd0f4b1b94266
SpStigma <GitHubStigma@gmail.com>
2023-07-18 01:46:13 -0700

    first attempt of array_oterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter
+ * each element of an array.
+ *
+ * @array: array
+ * @size: size
+ * @action: pointer action
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		action(array[i]);
+	}
+}
