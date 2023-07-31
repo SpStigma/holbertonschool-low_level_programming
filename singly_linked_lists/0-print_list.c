2bae875f8be14f2b5b7786e206b25e1c44e4c9ab
SpStigma <GitHubStigma@gmail.com>
2023-07-31 03:27:23 -0700

    fix: betty style

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,10 +1,14 @@
 #include "lists.h"
 
 /**
- *
- *
- *
- */
+* print_list - print str and the lenght of
+*              str and the number of node
+*
+* @h: first node
+*
+* Return: number of node
+*/
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
@@ -12,7 +16,7 @@ size_t print_list(const list_t *h)
 
 	while (ptr != NULL)
 	{
-		if(ptr->str != NULL)
+		if (ptr->str != NULL)
 		{
 			printf("[%u] %s\n", ptr->len, ptr->str);
 		}
@@ -25,4 +29,4 @@ size_t print_list(const list_t *h)
 
 	}
 	return (count);
-}	
+}
