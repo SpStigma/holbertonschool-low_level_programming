06e01d1e4f6b9aa1b0f928d648451ab0449d967d
SpStigma <GitHubStigma@gmail.com>
2023-08-01 05:21:54 -0700

    Betty style

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,16 +1,18 @@
 #include "lists.h"
 
 /**
+ * get_dnodeint_at_index - index to stop to the rigt node
+ * @head: pointer to the first node
+ * @index: the index to stop at the node
  *
- *
- *
+ * Return: the right node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *ptr = head;
 	unsigned int count = 0;
 
-	if(head == NULL)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
