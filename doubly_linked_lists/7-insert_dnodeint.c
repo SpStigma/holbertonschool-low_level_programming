63e3302746cf23ce18a7ae33025644de7b9cef72
SpStigma <GitHubStigma@gmail.com>
2023-08-01 12:42:15 -0700

    Fix: 40 lines

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,20 +21,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-	/* special case to add in the begining */
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
-	/* loop to iterating until the index or the end fof the list is reached */
 	while (ptr != NULL && count < idx - 1)
 	{
 		ptr = ptr->next;
 		count++;
 	}
-	/* check if the loop is terminated cause the index is found */
 	if (count == idx - 1)
 	{
 		new_node->prev = ptr;
@@ -47,13 +40,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		ptr->next = new_node;
 		return (new_node);
 	}
-	/* Condition to the case where ptr reach the end */
 	if (count == idx && ptr == NULL)
 	{
 		return (add_dnodeint_end(h, n));
 	}
 	else
-	{	/* if idx is out of range */
+	{
 		free(new_node);
 		return (NULL);
 	}
