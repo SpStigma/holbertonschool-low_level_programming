277bc0f81d69cd9caac9401700de02cc26fd4d23
SpStigma <GitHubStigma@gmail.com>
2023-08-01 01:58:07 -0700

    First attempt of task 2 doubly linked list first node

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - function that adds a new node doubly linked list at the beginning
+ * @head: pointer of pointer of the first node
+ * @n: to be store in the new node
+ *
+ * Return: new addres of new_node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL;
+
+	new_node =malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		new_node = NULL;
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	*head = new_node;
+	return (*head);
+}
