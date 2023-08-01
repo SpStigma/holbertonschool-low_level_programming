78788f9f6c233bcb464a8cf713a80c71a1fe151f
SpStigma <GitHubStigma@gmail.com>
2023-08-01 12:32:26 -0700

    Fix: add the fct to add a list at the end and little changement on the add everywhere

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,23 +21,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-
+	/* special case to add in the begining */
 	if (idx == 0)
 	{
-		add_dnodeint(h, n);
+		return (add_dnodeint(h, n));
 	}
-
-	while (ptr != NULL && count <= idx)
+	/* loop to iterating until the index or the end fof the list is reached */
+	while (ptr != NULL && count < idx - 1)
 	{
 		ptr = ptr->next;
 		count++;
 	}
-	if (ptr == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	else
+	/* check if the loop is terminated cause the index is found */
+	if (count == idx - 1)
 	{
 		new_node->prev = ptr;
 		new_node->next = ptr->next;
@@ -49,5 +45,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		ptr->next = new_node;
 		return (new_node);
 	}
+	/* Condition to the case where ptr reach the end */
+	if (count == idx && ptr == NULL)
+	{
+		return(add_dnodeint_end(h, n));
+	}
+	else
+	{	/* if idx is out of range */
+		free(new_node);
+		return (NULL);
+	}
 }
-
