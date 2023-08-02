94197a93b3ecf1e04ae202819388579c38e4bd25
SpStigma <GitHubStigma@gmail.com>
2023-08-02 10:36:37 -0700

    First attempt of the task 1 representation off binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: nbr to be representation binary
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+
+		for (int i = 0; i < sizeof(unsigned long int) * 8; i++)
+		{
+			putchar((n & mask) ? '1' : '0');
+			mask >>= 1;
+		}
+	putchar('\n');
+}
