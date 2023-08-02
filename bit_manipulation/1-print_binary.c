76dc4e8785e54a85d79a2936e1205dca7d1634a3
SpStigma <GitHubStigma@gmail.com>
2023-08-02 10:43:06 -0700

    Fix: Thx Fab

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -8,12 +8,26 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-	unsigned long int i = 0;
+	int found_one = 0;
 
-		for (; i < sizeof(unsigned long int) * 8; i++)
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (mask)
+	{
+		if (n & mask)
 		{
-			putchar((n & mask) ? '1' : '0');
-			mask >>= 1;
+			_putchar('1');
+			found_one = 1;
 		}
-	putchar('\n');
+		else if (found_one)
+		{
+			_putchar('0');
+		}
+
+		mask >>= 1;
+	}
 }
