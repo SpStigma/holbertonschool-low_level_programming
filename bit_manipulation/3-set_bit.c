0daba1afe6edc416071774b8f13b05aa6aed3ed4
SpStigma <GitHubStigma@gmail.com>
2023-08-03 01:38:51 -0700

    fix: betty style

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * set_bit - function that sets the value of a bit to 1 at a given index.
+ * set_bit - function that sets the value of a bit to 1 at a given index
  * @n: nit given
  * @index: index
  *
  * Return: vale 1
  */
-int set_bit(unsigned long int *n, unsigned int index) 
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	 unsigned long int mask = 1UL << index;
+	unsigned long int mask = 1UL << index;
 
-    if (index >= sizeof(unsigned long int) * 8) 
-    {
-        return (-1);
-    }
-    *n |= mask;
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	*n |= mask;
 
-    return (1);
+	return (1);
 }
