d157674bcce3a1dd68516cf215b72914ca1f7b63
SpStigma <GitHubStigma@gmail.com>
2023-08-03 01:44:29 -0700

    First attempt of task 4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - Sets the value of a bit to 0 at a given index.
+ * @n: Pointer to the unsigned long int number.
+ * @index: The index of the bit to set (starting from 0).
+ *
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1UL << index;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	mask = ~mask;
+	*n &= mask;
+	return (1);
+}
