7b29cac215105ef2848ab05a139d0c62a5ef407f
SpStigma <GitHubStigma@gmail.com>
2023-08-07 15:07:48 -0700

    Fix: try to get the good output on error messages 99

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -53,7 +53,6 @@ int copy_file_content(int source_fd, int destination_fd)
 	{
 		if (write(destination_fd, buffer, bytes_read) != bytes_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %d\n", destination_fd);
 			exit(99);
 		}
 	}
