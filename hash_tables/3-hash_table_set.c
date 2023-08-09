761260330d0f87c0fef781369ca5559f47654e85
SpStigma <GitHubStigma@gmail.com>
2023-08-09 02:09:45 -0700

    Fix:Test new code

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,22 +17,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	i = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
+	new_node->next = ht->array[i];
 
-	if (ht->array[i] == NULL)
-	{
-		ht->array[i] = new_node;
-	}
-	else
-	{
-		temp_node = ht->array[i];
-		while (temp_node->next != NULL)
-		{
-			temp_node = temp_node->next;
-		}
-		temp_node->next = new_node;
-	}
+	ht->array[i] = new_node;
+
 	return (1);
 }
