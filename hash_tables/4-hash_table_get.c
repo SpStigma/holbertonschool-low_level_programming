bc0b83e7bb4b057b1b571aa88bc0a51161fa3d30
SpStigma <GitHubStigma@gmail.com>
2023-08-09 04:52:56 -0700

    Fix: betty style and add a new condition

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,14 +1,22 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_get - retrieves a value associated with a key from a hash table.
+ * @ht: A pointer to the hash table
+ * @key: the key to search for in the hash table
  *
- *
- *
+ * Return: The value associated with the key, or null if key is not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned int i = 0;
 	hash_node_t *node = NULL;
+
+
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
 
 	i = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[i];
