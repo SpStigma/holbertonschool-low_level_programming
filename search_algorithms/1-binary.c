91cd745f38d0c02f25849106c4e6a06281df527e
SpStigma <githubstigma@gmail.com>
2023-09-19 16:03:16 +0200

    Fix: betty style.

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -14,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 	size_t low = 0;
 	size_t high = size - 1;
 	size_t i = 0;
-    size_t mid = 0;
+	size_t mid = 0;
 
 	if (array == NULL)
 		return (-1);
@@ -33,7 +33,7 @@ int binary_search(int *array, size_t size, int value)
 			else
 				printf("\n");
 		}
-        mid = (low + high) / 2;
+		mid = (low + high) / 2;
 
 		if (array[mid] == value)
 			return (mid);
